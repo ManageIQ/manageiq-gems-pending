76435354c00f7a8a23a96f7b123eba8e9919d78f
Adam Grare <agrare@redhat.com>
2017-05-19 13:21:07 -0400

    Remove VMwareWebServices from codeclimate and coverage

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -1,7 +1,7 @@
 require 'active_support/core_ext/kernel/reporting'
 
 # Require all ruby files for accuracte test coverage reports
-EXCLUSIONS_LIST = %w(/bin/ /ext/ /spec/ /test/ /vendor/ appliance_console.rb test.rb require_with_logging.rb VixDiskLibServer.rb VMwareWebService/wsdl41 )
+EXCLUSIONS_LIST = %w(/bin/ /ext/ /spec/ /test/ /vendor/ appliance_console.rb test.rb require_with_logging.rb VixDiskLibServer.rb)
 Dir.glob(ManageIQ::Gems::Pending.root.join("**", "*.rb")).each do |file|
   next if EXCLUSIONS_LIST.any? { |exclusion| file.include?(exclusion) }
   begin

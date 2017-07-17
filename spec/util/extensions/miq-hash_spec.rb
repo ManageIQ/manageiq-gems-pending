4a4739cd914678e72fe6b931c49804b4bfdddb90
Brandon Dunne <bdunne@redhat.com>
2017-07-17 15:43:14 -0400

    Extract hash sorting to more_core_extensions
    https://github.com/ManageIQ/more_core_extensions/pull/46

--- spec/util/extensions/miq-hash_spec.rb
+++ spec/util/extensions/miq-hash_spec.rb
@@ -17,24 +17,4 @@ describe Hash do
     expect(s2).to eq(s)
     expect(s2.sub_str).to eq(s.sub_str)
   end
-
-  it "#sort!" do
-    h = {:x => 1, :b => 2, :y => 3, :a => 4}
-    h_id = h.object_id
-
-    h.sort!
-
-    expect(h.keys).to eq([:a, :b, :x, :y])
-    expect(h.object_id).to eq(h_id)
-  end
-
-  it "#sort_by!" do
-    h = {:x => 1, :b => 2, :y => 3, :a => 4}
-    h_id = h.object_id
-
-    h.sort_by! { |k, _v| k }
-
-    expect(h.keys).to eq([:a, :b, :x, :y])
-    expect(h.object_id).to eq(h_id)
-  end
 end

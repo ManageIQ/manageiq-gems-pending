b98146befbd36063ea280bf75f91fb05fe1830e8
Beni Cherniavsky-Paskin <cben@redhat.com>
2017-08-21 13:58:30 +0300

    Instrumented test for Timeout breaking .realtime_block cleanup
    Slow.  Prints detailed success/fail stats per execution point.
    Test doesn't actually fail.
    To be replaced with simpler test based on this data.

--- spec/util/extensions/miq-benchmark_spec.rb
+++ spec/util/extensions/miq-benchmark_spec.rb
@@ -1,8 +1,13 @@
 require 'util/extensions/miq-benchmark'
 require 'timecop'
+require 'timeout'
 
 describe Benchmark do
   after(:each) { Timecop.return }
+  after(:each) do
+    # Isolate other tests
+    Benchmark.delete_current_realtime if Benchmark.in_realtime_block?
+  end
 
   it '.realtime_store' do
     timings = {}
@@ -57,5 +62,43 @@ describe Benchmark do
       expect(Benchmark.in_realtime_block?).to be_truthy
     end
     expect(Benchmark.in_realtime_block?).to be_falsey
+  end
+
+  it "Timeout raising within .realtime_block" do
+    expect(Benchmark.in_realtime_block?).to be_falsey
+
+    places_outcomes = {}
+    1000.times do |i|
+      begin
+        # keep entering/exiting, abort ASAP
+        Timeout.timeout(1e-9) do
+          loop do
+            Benchmark.realtime_block(:test1) do
+              Benchmark.realtime_block(:test2) do
+              end
+              "result"
+            end
+          end
+        end
+      rescue Timeout::Error => e
+        # sortable compact stacks: realtime_block and children, outer first, strip directories, pad line numbers
+        interesting_depth = e.backtrace.rindex { |s| s =~ /in `realtime_block'/ } || 5
+        where = e.backtrace[0..interesting_depth].reverse
+        where = where.collect { |s| s.sub(%r{/.*/}, '').sub(/\d+:/) { |linenum| '%03d:' % linenum.to_i } }
+        places_outcomes[where] ||= {:cleaned => 0, :failed => 0}
+        if Benchmark.in_realtime_block?
+          places_outcomes[where][:failed] += 1
+          Benchmark.delete_current_realtime
+        else
+          places_outcomes[where][:cleaned] += 1
+        end
+      else
+        fail "impossible: escaped infinite loop without exception"
+      end
+    end
+
+    places_outcomes.sort.each do |where, outcomes|
+      puts "cleaned\t#{outcomes[:cleaned]}\tfailed\t#{outcomes[:failed]}\t#{where.join(' -> ')}"
+    end
   end
 end

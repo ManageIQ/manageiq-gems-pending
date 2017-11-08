76700bd7592aa4108bb1c78a747371a08ebac495
Joe Rafaniello <jrafanie@users.noreply.github.com>
2017-11-08 16:11:01 -0500

    Merge pull request #309 from bdunne/benchmark_logger
    Use $log if it's available, otherwise write to $stderr
    (cherry picked from commit cafb949f42b2214c1d7551f870a69be0814c1f5d)

--- spec/util/extensions/miq-benchmark_spec.rb
+++ spec/util/extensions/miq-benchmark_spec.rb
@@ -65,6 +65,7 @@ describe Benchmark do
   end
 
   it '.realtime_block with an Exception aborting outermost block' do
+    expect(Benchmark.send(:logger)).to receive(:info).with(/Exception in realtime_block :test1 - Timings: {:test2=>5\.\d*, :test1=>7\.\d*}/)
     expect do
       Benchmark.realtime_block(:test1) do
         Timecop.travel(2.1)
@@ -74,7 +75,6 @@ describe Benchmark do
         end
       end
     end.to raise_exception(Exception)
-      .and output(/Exception in realtime_block :test1 - Timings: {:test2=>5\.\d*, :test1=>7\.\d*}/).to_stderr
 
     expect(Benchmark.in_realtime_block?).to be_falsey
   end

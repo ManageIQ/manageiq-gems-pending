6d524646c1c3ea8d4712356f93ee17255f441e18
NickLaMuro <nicklamuro@gmail.com>
2020-01-23 11:14:56 -0600

    [MiqUtil.runcmd] Use AwesomeSpawn
    A few compromises were made as part of this patch to ideally avoid old
    callers to still function.
    
    - The `test = false` was preserved, though probably should be deprecated
      and removed in the future.
    - `AwesomeSpawn.run!` is used instead of raising an error manually
      overwritten if desired)
    - The output is the return value
    
    TODO
    ----
    
    Currently when a cmd errors, nothing will be displayed in the
    logger since all of the error output is redirected to STDOUT.
    
    Possibly as a solution, rescuing the error and raising it again will
    allow us to log what is needed properly, and but still maintain most of
    the features of `AwesomeSpawn.run!`
    
    Also, allow STDERR to be merged into STDOUT as before by default
    (can't be done currently do to using `Open3.capture3`).

--- spec/util/runcmd_spec.rb
+++ spec/util/runcmd_spec.rb
@@ -10,7 +10,6 @@ describe 'MiqUtil.runcmd' do
   # wrap in "/bin/sh -c" so this set of specs is shared between the original
   # implementation and AwesomeSpawn.run!
   def runcmd(cmd_str, args = {}, test = false)
-    cmd_str = "/bin/sh -c '#{cmd_str}'" unless test
     MiqUtil.runcmd(cmd_str, test)
   end
 
@@ -19,17 +18,30 @@ describe 'MiqUtil.runcmd' do
   end
 
   it "raises the resulting output" do
-    expect { runcmd(failed_cmd) }.to raise_error(RuntimeError)
+    expect { runcmd(failed_cmd) }.to raise_error(AwesomeSpawn::CommandResultError)
   end
 
   context "test mode" do
     before { disable_spawning }
+    let(:cmd)    { "true --opt1 foo --opt2=bar -fkvx arg1 arg2" }
+    let(:output) { "#{cmd}: Test output" }
 
     it "prints the executed command" do
-      cmd    = "true --opt1 foo --opt2=bar -fkvx arg1 arg2"
-      output = "#{cmd}: Test output"
-
       expect(runcmd(cmd, nil, true)).to eq output
+    end
+
+    it "works with a params hash for cmd_args" do
+      args = {
+        :opt1 => "foo",
+        :opt2 => "bar",
+        :f    => nil,
+        :k    => nil,
+        :v    => nil,
+        :x    => nil,
+        nil   => %w[arg1 arg2]
+      }
+
+      expect(runcmd(cmd, {:params => args}, true)).to eq output
     end
   end
 end

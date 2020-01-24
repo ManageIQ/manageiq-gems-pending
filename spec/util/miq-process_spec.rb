eb11183dd7ef75fb2f14e3e3659202f5906cefd1
Jason Frey <jfrey@redhat.com>
2020-01-24 15:50:33 -0500

    Mark as pending on MacOS when /proc is not accessible

--- spec/util/miq-process_spec.rb
+++ spec/util/miq-process_spec.rb
@@ -13,6 +13,13 @@ describe MiqProcess do
     end
 
     it "normal case" do
+      if RbConfig::CONFIG['host_os'] =~ /darwin/
+        begin
+          Sys::ProcTable.ps(Process.pid)
+        rescue Errno::ENOENT
+          pending "fails on MacOS due to absence of /proc"
+        end
+      end
       expect(described_class.command_line(Process.pid)).not_to be_empty
     end
   end

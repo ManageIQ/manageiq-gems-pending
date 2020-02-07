d41bd6715225368af043653b88b07c6235ba8240
Jason Frey <jfrey@redhat.com>
2020-02-07 16:04:57 -0500

    Merge branch 'cve_2019-14894-user_awesomespawn_for_runcmd' into '5.11.z'
    Replace `runcmd` with AwesomeSpawn
    
    See merge request cloudforms/cfme-gems-pending!3
    
    (cherry picked from commit 26183da505d0140a0840084db36b9245eb4f421d)
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1775128

--- lib/gems/pending/util/runcmd.rb
+++ lib/gems/pending/util/runcmd.rb
@@ -1,14 +1,19 @@
+require 'awesome_spawn'
+
 class MiqUtil
-  def self.runcmd(cmd_str, test = false)
-    if !test
-      rv = `#{cmd_str} 2>&1`
-      if $? != 0
-        raise rv
-      end
-    else
-      rv = "#{cmd_str}: Test output"
+  def self.runcmd(cmd, cmd_args = {}, test = false)
+    unless [Hash, Array, String].any? { |klass| cmd_args.kind_of?(klass) }
+      test     = cmd_args
+      cmd_args = {}
     end
 
-    rv
+    args = { :combined_output => true }.merge(cmd_args)
+    if !test
+      rv = AwesomeSpawn.run!(cmd, args)
+      rv.output
+    else
+      cmd_str = AwesomeSpawn::CommandLineBuilder.new.build(cmd, args[:params])
+      "#{cmd_str}: Test output"
+    end
   end
 end
---
+++ spec/util/runcmd_spec.rb
@@ -0,0 +1,47 @@
+require 'util/runcmd'
+require 'awesome_spawn/spec_helper'
+
+describe 'MiqUtil.runcmd' do
+  include AwesomeSpawn::SpecHelper
+
+  let(:cmd)        { 'echo Hi; echo Hitoo 1>&2' }
+  let(:failed_cmd) { 'echo Hi; false --foo --bar -fkxv arg1' }
+
+  # wrap in "/bin/sh -c" so this set of specs is shared between the original
+  # implementation and AwesomeSpawn.run!
+  def runcmd(cmd_str, args = {}, test = false)
+    MiqUtil.runcmd(cmd_str, test)
+  end
+
+  it "returns stdout & stderr as output" do
+    expect(runcmd(cmd)).to eq("Hi\nHitoo\n")
+  end
+
+  it "raises the resulting output" do
+    expect { runcmd(failed_cmd) }.to raise_error(AwesomeSpawn::CommandResultError)
+  end
+
+  context "test mode" do
+    before { disable_spawning }
+    let(:cmd)    { "true --opt1 foo --opt2=bar -fkvx arg1 arg2" }
+    let(:output) { "#{cmd}: Test output" }
+
+    it "prints the executed command" do
+      expect(runcmd(cmd, nil, true)).to eq output
+    end
+
+    it "works with a params hash for cmd_args" do
+      args = {
+        :opt1 => "foo",
+        :opt2 => "bar",
+        :f    => nil,
+        :k    => nil,
+        :v    => nil,
+        :x    => nil,
+        nil   => %w[arg1 arg2]
+      }
+
+      expect(runcmd(cmd, {:params => args}, true)).to eq output
+    end
+  end
+end

0e2b03cda815b9ac7d4b8a1885af1e74f0a78a6a
Daniel Berger <djberg96@gmail.com>
2020-06-15 16:57:24 -0400

    Remove miq-kernel / require_relative since it now works as desired in core Ruby.

--- lib/gems/pending/util/extensions/miq-kernel.rb
+++
@@ -1,7 +0,0 @@
-module Kernel
-  # Replaces Kernel's require_relative to allow it to be used in irb and eval
-  # See: http://bugs.ruby-lang.org/issues/4487
-  def require_relative(path)
-    require File.join(File.dirname(caller[0]), path.to_str)
-  end
-end
